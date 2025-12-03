f3b53e4466fa4e0f1cf6ff0a86f7f72cbb80e29d
Ben Noyes <noyesbenjamin@gmail.com>
2025-12-03 13:24:16 -0500

    updated with ec2 info

--- xmas/settings.py
+++ xmas/settings.py
@@ -16,6 +16,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 SITE_ROOT = os.path.join(BASE_DIR, "site")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = "/static/"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
