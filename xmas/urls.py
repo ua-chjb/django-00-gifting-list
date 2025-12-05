c1ecc7259540efd9f78639c7fe536537240f5ffe
Ben Noyes <noyesbenjamin@gmail.com>
2025-12-04 19:45:07 -0500

    debugging logout functionality

--- xmas/urls.py
+++ xmas/urls.py
@@ -2,13 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
-class GetLogoutView(auth_views.LogoutView):
-    http_method_names = ["get", "post", "options"]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
-    path("logout/", GetLogoutView.as_view(), name="logout"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("", include("main_list.urls"))
 ]
